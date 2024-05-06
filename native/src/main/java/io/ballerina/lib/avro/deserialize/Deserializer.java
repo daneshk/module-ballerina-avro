d010c1338178e8c2bffc10455cceb63f9218043a
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2024-05-06 07:44:19 +0530

    Add visitable classes for each Avro schema type in deserialization

---
+++ native/src/main/java/io/ballerina/lib/avro/deserialize/Deserializer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com)
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.lib.avro.deserialize;
+
+import io.ballerina.lib.avro.visitor.DeserializeVisitor;
+import io.ballerina.runtime.api.types.Type;
+import io.ballerina.runtime.api.utils.TypeUtils;
+import org.apache.avro.Schema;
+
+public abstract class Deserializer {
+
+    private final Schema schema;
+    private final Type type;
+
+    public Deserializer() {
+        this.schema = null;
+        this.type = null;
+    }
+
+    public Deserializer(Type type) {
+        this.schema = null;
+        this.type = TypeUtils.getReferredType(type);
+    }
+
+    public Deserializer(Schema schema, Type type) {
+        this.schema = new Schema.Parser().parse(schema.toString());
+        this.type = TypeUtils.getReferredType(type);
+    }
+
+    protected Schema getSchema() {
+        return schema;
+    }
+
+    protected Type getType() {
+        return type;
+    }
+
+    public abstract Object fromAvroMessage(DeserializeVisitor visitor, Object data, Schema schema) throws Exception;
+}
---
+++ native/src/main/java/io/ballerina/lib/avro/deserialize/RecordDeserializer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com)
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.lib.avro.deserialize;
+
+import io.ballerina.lib.avro.visitor.DeserializeVisitor;
+import io.ballerina.runtime.api.types.Type;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+
+public class RecordDeserializer extends Deserializer {
+
+    public RecordDeserializer(Schema schema, Type type) {
+        super(schema, type);
+    }
+
+    @Override
+    public Object fromAvroMessage(DeserializeVisitor visitor, Object data, Schema schema) throws Exception {
+        return visitor.visitRecords(getType(), getSchema(), (GenericRecord) data);
+    }
+}
